3ed45b618face5ccc061162e1360615fca0929df
grgr_smrkv <grgrsmrkv@gmail.com>
2023-05-22 10:22:10 +0300

    crash test

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnTestData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("PostmanEchoTest data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(400)
+                .body("data", equalTo("PostmanEchoTest data"))
+        ;
+    }
+}
